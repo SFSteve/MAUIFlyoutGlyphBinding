c8521457a6d65746b505e52496c36d2a5be42a0e
storefeedersteve <steve@storefeeder.com>
2023-09-20 14:20:28 +0100

    Initial commit

---
+++ FlyoutGlyphTest/FlyoutGlyphTest/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FlyoutGlyphTest;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
